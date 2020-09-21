b105f99034ca414fc154bec3c8e4a96a74764be3
Ansul Thakur <ansultkr@iitk.ac.in>
2020-09-21 18:11:50 +0530

    4:Here MathCheat constructor is called only once therby preventing overloading

--- src/com/ansul/thakur/MathCheat.java
+++ src/com/ansul/thakur/MathCheat.java
@@ -1,6 +1,11 @@
 package com.ansul.thakur;
 
 public class MathCheat {
+	
+	public MathCheat() {
+		System.out.println("MathCheat class constructor called");
+	}
+	
 	public void mathCheat() {
 		System.out.println("Math cheatingg started...");
 		
